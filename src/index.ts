5504cfdc54dd53bb73564e13f6cece8a0c8eb78a
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2022-05-09 16:14:59 +0800

    Export WalletStorage interface to implement custom storages in apps

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { BrowserLocalStorage } from "./persistence/implementations/BrowserLocalStorage";
+import { WalletStorage } from "./persistence/WalletStorage";
 import { MainchainSubWallet } from "./wallet/MainchainSubWallet";
 import { MasterWallet } from "./wallet/MasterWallet";
 import { MasterWalletManager } from "./wallet/MasterWalletManager";
@@ -7,12 +8,17 @@ import { HDKey, KeySpec } from "./walletcore/hdkey";
 import { Mnemonic } from "./walletcore/mnemonic";
 
 export {
+  WalletStorage,
+  BrowserLocalStorage,
+
+  // Crypto
   Mnemonic,
   KeySpec,
   HDKey,
+
+  // Wallets
   MasterWallet,
   MasterWalletManager,
-  BrowserLocalStorage,
   SubWallet,
   MainchainSubWallet
 };
