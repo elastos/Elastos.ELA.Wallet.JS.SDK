8fcd0b321a8f3288c970503583c14d1cf100c308
Billie Zhang <billiecoder@gmail.com>
2022-05-12 22:59:22 +0800

    correct WalletErrorException

--- src/common/exceptions/walleterror.exception.ts
+++ src/common/exceptions/walleterror.exception.ts
@@ -1,8 +1,6 @@
 export class WalletErrorException extends Error {
   constructor(public code: number, public reason: string) {
     super(`Code: ${code}, Reason: ${reason}`);
-    this.code = code;
-    this.reason = reason;
     this.name = this.constructor.name;
   }
 }
