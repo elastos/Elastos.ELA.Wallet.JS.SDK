d9ca10809cb710d594d3baee100a55a2d1dd9494
Benjamin Piette <benjaminpiette@elastos-fusion.org>
2022-10-17 13:43:04 +0800

    Export UTXO types + v1.0.2

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,52 @@
-import { WalletStorage } from "./persistence/WalletStorage";
-import { MainchainSubWallet } from "./wallet/MainchainSubWallet";
-import { IDChainSubWallet } from "./wallet/IDChainSubWallet";
-import { MasterWallet, SubWalletInstance } from "./wallet/MasterWallet";
-import { MasterWalletManager } from "./wallet/MasterWalletManager";
-import { SubWallet } from "./wallet/SubWallet";
-import { HDKey, KeySpec } from "./walletcore/hdkey";
-import { Mnemonic } from "./walletcore/mnemonic";
-import { WalletErrorException } from "./common/exceptions/walleterror.exception";
 import { AccountBasicInfo, AccountPubKeyInfo } from "./account/Account";
-import { SignedInfo } from "./transactions/Program";
-import { EncodedTx } from "./wallet/IElastosBaseSubWallet";
-import { SigningPublicKeyInfo } from "./wallet/ElastosBaseSubWallet";
+import { PublickeysInfo } from "./account/SubAccount";
+import { get32BytesOfBNAsHexString } from "./common/bnutils";
+import { ByteStream } from "./common/bytestream";
+import { WalletErrorException } from "./common/exceptions/walleterror.exception";
+import { ConfigInfo } from "./config";
+import { KeystoreStorage } from "./persistence/KeystoreStorage";
 import { LocalStoreInfo } from "./persistence/LocalStore";
-import {
-  VoteContentInfo,
-  VoteContentType
-} from "./transactions/payload/OutputPayload/PayloadVote";
-import { CRInfoJson, CRInfoPayload } from "./transactions/payload/CRInfo";
+import { WalletStorage } from "./persistence/WalletStorage";
+import { CancelProducerInfo } from "./transactions/payload/CancelProducer";
+import { CancelVotesInfo } from "./transactions/payload/CancelVotes";
 import { CRCouncilMemberClaimNodeInfo } from "./transactions/payload/CRCouncilMemberClaimNode";
 import {
-  NormalProposalOwnerInfo,
-  CRCProposalInfo,
-  ChangeProposalOwnerInfo,
-  TerminateProposalOwnerInfo,
-  SecretaryElectionInfo,
-  ReserveCustomIDOwnerInfo,
-  ReceiveCustomIDOwnerInfo,
-  ChangeCustomIDFeeOwnerInfo,
-  RegisterSidechainProposalInfo,
-  UpgradeCodeProposalInfo,
-  CRCProposalType
+  ChangeCustomIDFeeOwnerInfo, ChangeProposalOwnerInfo, CRCProposalInfo, CRCProposalType, NormalProposalOwnerInfo, ReceiveCustomIDOwnerInfo, RegisterSidechainProposalInfo, ReserveCustomIDOwnerInfo, SecretaryElectionInfo, TerminateProposalOwnerInfo, UpgradeCodeProposalInfo
 } from "./transactions/payload/CRCProposal";
-import { CRCProposalReviewInfo } from "./transactions/payload/CRCProposalReview";
-import { VoteResult } from "./transactions/payload/CRCProposalReview";
+import { CRCProposalReviewInfo, VoteResult } from "./transactions/payload/CRCProposalReview";
 import {
   CRCProposalTrackingInfo,
   CRCProposalTrackingType
 } from "./transactions/payload/CRCProposalTracking";
 import { CRCProposalWithdrawInfo } from "./transactions/payload/CRCProposalWithdraw";
-import { Address } from "./walletcore/Address";
-import { get32BytesOfBNAsHexString } from "./common/bnutils";
-import { PayloadStakeInfo } from "./transactions/payload/OutputPayload/PayloadStake";
-import { VotingInfo, VotesContentInfo } from "./transactions/payload/Voting";
-import { UnstakeInfo } from "./transactions/payload/Unstake";
+import { CRInfoJson, CRInfoPayload } from "./transactions/payload/CRInfo";
 import { DPoSV2ClaimRewardInfo } from "./transactions/payload/DPoSV2ClaimReward";
-import { CancelVotesInfo } from "./transactions/payload/CancelVotes";
-import { SHA256 } from "./walletcore/sha256";
-import { ByteStream } from "./common/bytestream";
-import { KeystoreStorage } from "./persistence/KeystoreStorage";
-import { KeystoreInfo, KeyStore } from "./walletcore/keystore";
-import { ConfigInfo } from "./config";
-import { PublickeysInfo } from "./account/SubAccount";
-import { CancelProducerInfo } from "./transactions/payload/CancelProducer";
+import { PayloadStakeInfo } from "./transactions/payload/OutputPayload/PayloadStake";
+import {
+  VoteContentInfo,
+  VoteContentType
+} from "./transactions/payload/OutputPayload/PayloadVote";
 import { ProducerInfoJson } from "./transactions/payload/ProducerInfo";
 import { UnregisterCRPayload } from "./transactions/payload/UnregisterCR";
-import { JSONObject, json } from "./types";
+import { UnstakeInfo } from "./transactions/payload/Unstake";
+import { VotesContentInfo, VotingInfo } from "./transactions/payload/Voting";
+import { SignedInfo } from "./transactions/Program";
+import { json, JSONObject } from "./types";
+import { SigningPublicKeyInfo } from "./wallet/ElastosBaseSubWallet";
+import { IDChainSubWallet } from "./wallet/IDChainSubWallet";
+import { EncodedTx } from "./wallet/IElastosBaseSubWallet";
+import { MainchainSubWallet } from "./wallet/MainchainSubWallet";
+import { MasterWallet, SubWalletInstance } from "./wallet/MasterWallet";
+import { MasterWalletManager } from "./wallet/MasterWalletManager";
+import { SubWallet } from "./wallet/SubWallet";
+import { UTXOArray, UTXOInput, UTXOSet } from "./wallet/UTXO";
+import { Address } from "./walletcore/Address";
+import { HDKey, KeySpec } from "./walletcore/hdkey";
+import { KeyStore, KeystoreInfo } from "./walletcore/keystore";
+import { Mnemonic } from "./walletcore/mnemonic";
+import { SHA256 } from "./walletcore/sha256";
 
 export * from "./transactions/payload";
-
 export {
   Address,
   Mnemonic,
@@ -78,7 +67,6 @@ export {
   ByteStream,
   KeyStore
 };
-
 export type {
   json,
   JSONObject,
@@ -119,5 +107,9 @@ export type {
   PublickeysInfo,
   CancelProducerInfo,
   ProducerInfoJson,
-  UnregisterCRPayload
+  UnregisterCRPayload,
+  UTXOInput,
+  UTXOArray,
+  UTXOSet
 };
+
